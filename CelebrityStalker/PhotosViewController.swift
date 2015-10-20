fe8376d1b0279b7d5d62860316294c4734d49d32
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-20 16:08:15 -0400

    Flickr api passing data back and am picking random photo from feed to fetch url

--- CelebrityStalker/PhotosViewController.swift
+++ CelebrityStalker/PhotosViewController.swift
@@ -36,8 +36,8 @@ class PhotosViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        celebrityNameLabel.text = "Lionel Ritchie"
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,20 +47,119 @@ class PhotosViewController: UIViewController {
     
     
     @IBAction func getNextCelebrityPhoto(sender: AnyObject) {
+        // Hardcode the arguments
+        let methodArguments: [String: String!] = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "text": "Lionel Ritchie",
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        /* Call the Flickr API with these arguments */
+        getImageFromFlickrByCelebrityName(methodArguments)
     }
     
     
+    // MARK: Flickr API
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getImageFromFlickrByCelebrityName(methodArguments: [String : AnyObject]) {
+        
+        /* 3 - Get the shared NSURLSession to faciliate network activity */
+        let session = NSURLSession.sharedSession()
+        
+        /* 4 - Create the NSURLRequest using properly escaped URL */
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        /* 5 - Create NSURLSessionDataTask and completion handler */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as! [String : AnyObject]
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? [String:AnyObject] else {
+                print("Cannot find keys 'photos' in \(parsedResult)")
+                return
+            }
+            
+            print(photosDictionary)
+            let photoArray = photosDictionary["photo"] as! [[String: AnyObject]]
+            let randomIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            let myPhoto = photoArray[randomIndex] 
+            let myURL = myPhoto["url_m"]
+            print(myURL)
+        
+            
+        }
+        
+        task.resume()
     }
-    */
+
+    
+    
+    
+    
+    // MARK: Escape HTML Parameters
+    
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
 
 }
